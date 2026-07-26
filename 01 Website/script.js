3c359d6a4003d1fa75aceb7f1091f003009530f6
NeuralMesswork <lie.p@hotmail.com>
2026-07-26 23:07:20 +0200

    vers2

---
+++ 01 Website/script.js
@@ -0,0 +1,81 @@
+//Fun facts
+
+function visaSvar() {
+    const svar = [
+        "webbsidan är frontendutvecklad i HTML, CSS och Javascript, Källkodhantering i GitHub, deployment via hostingplattform och exponerad genom egen domän med kryptering.",
+        "under min masteruppsats gick det åt mer än 1060 koppar kaffe.",
+        "jag älskar Lord of the rings så mycket att jag läst böckerna minst en gång vartannat år sedan högstadiet.",
+        "jag gärna lär mig nya tekniska verktyg bara för att förstå hur de fungerar. Precis som jag gjorde under mitt webbplats-bygge.",
+        "jag är team mörkt tema.",
+        "jag inte kan vissla.",
+        "jag har läst färdigt 405 hp på universitetet, vilket motsvarar ungefär 10 800 timmar",
+        "min favoritårstid är vinter.",
+        "när andra samlar frimärken, samlar jag på artiklar att läsa senare.",
+        "jag brukar läsa recensionerna innan jag bestämmer mig för något."
+    ];
+
+    const slump = Math.floor(Math.random() * svar.length);
+
+    document.getElementById("svar").textContent = svar[slump];
+}
+
+// Achievements
+
+let klick = 0;
+
+console.log("Achievement-system startat");
+
+document.addEventListener("click", function() {
+    klick++;
+
+    if (klick === 20) {
+        visaAchievement("🏆 Achievement unlocked: Nyfiken besökare");
+    }
+
+    if (klick === 30) {
+        visaAchievement("🕵️ Achievement unlocked: Digital detektiv");
+    }
+
+    if (klick === 50) {
+        visaAchievement("👑 Achievement unlocked: Klickmästare");
+    }
+
+    if (klick === 100) {
+        visaAchievement("☕ Achievement unlocked: Koffeinklassad användare");
+    }
+    
+    if (klick === 200) {
+        visaAchievement("🚀 Achievement unlocked: Explorer Mode");
+    }
+    
+    if (klick === 300) {
+        visaAchievement("🐉 Secret achievement unlocked: Här finns inga fler achievements... Eller?");
+    }
+    
+    if (klick === 400) {
+        visaAchievement(" Secret achievement unlocked: Grattis. Du klickade 400 gånger för en potatis");
+    }
+
+    if (klick === 500) {
+        visaAchievement("🕳️ Achievement unlocked: Alice, du följde kaninhålet hela vägen ner");
+    }
+    
+    if (klick === 800) {
+        visaAchievement("🦄 Secret achievement unlocked: Ingen förväntade sig att du skulle komma hit");
+    }
+    
+    if (klick === 1000) {
+        visaAchievement("👸 Achievement unlocked: Tack Mario! Din strävan är över");
+    }
+    });
+
+function visaAchievement(text) {
+    const achievement = document.getElementById("achievement");
+
+    achievement.textContent = text;
+    achievement.style.display = "block";
+    
+    setTimeout(() => {
+        achievement.style.display = "none";
+    }, 7000);
+}
